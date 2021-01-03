b1c6dd9e7e7dcec0f097bbde83c892e3a59c6c81
Julio Cesar Westarb Jr <jwestarb@gmail.com>
2021-01-03 14:44:16 -0300

    Configurado Typescript e remoção de arquivos não utilizados

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Boilerplate Next.js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Boilerplate Next.js
+        </h1>
+      </main>
+
+      <footer>
+          Powered by jwestarb!
+      </footer>
+    </div>
+  )
+}
