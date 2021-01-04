ede187ea2e49dcf57bef319d452b9af542fbfc7b
Julio Cesar Westarb Jr <jwestarb@gmail.com>
2021-01-03 22:59:04 -0300

    Adiciona Style Components com GlobalStyles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Boilerplate Next.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>Boilerplate Next.js</h1>
-      </main>
-
-      <footer>Powered by jwestarb!</footer>
-    </div>
-  )
+  return <Main />
 }
